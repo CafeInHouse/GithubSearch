edb2b03b95c42f984be4bb1a43e0489070f66ff4
linsaeng <duwjdtmd91@gmail.com>
2024-03-29 21:21:25 +0900

    #1[프로젝트] 프로젝트를 생성합니다. - hello Cafein

---
+++ GithubSearchApp/GithubSearchApp/GithubSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubSearchAppApp.swift
+//  GithubSearchApp
+//
+//  Created by saeng lin on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct GithubSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
